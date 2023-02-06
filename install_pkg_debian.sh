e0383d4cfa4e7e6e74a82236654b98d890a3559b
Z L <zian.li.1996@outlook.com>
2023-02-06 23:36:56 +0100

    Add auto build - installation scripts

---
+++ install_pkg_debian.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update
+apt upgrade
+
+apt install -y build-essential
+apt install -y python3 python-is-python3
+apt install -y python3-opencv
+apt install -y wget curl
+apt install -y cmake pkg-config
+apt install -y leptonica-progs libleptonica-dev libicu-dev libidn11-dev libpango1.0-dev libcairo2-dev libcurl4-openssl-dev libarchive-dev libcurlpp-dev libssh2-1-dev libssl-dev
